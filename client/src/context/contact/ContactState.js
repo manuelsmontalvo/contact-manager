65d59948e75c320505323152b9b52db7132f60eb
Manuel  Montalvo <manny@Manuels-MacBook-Pro.local>
2021-05-26 16:30:16 -0400

    app done

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -76,6 +76,30 @@ const ContactsState = (props) => {
     }
   };
 
+  // Update Contact
+  const updateContact = async (contact) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+
+      dispatch({ type: UPDATE_CONTACT, payload: res.data });
+    } catch (error) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: error.response.msg,
+      });
+    }
+  };
+
   // Clear Contacts
   const clearContacts = () => {
     dispatch({ type: CLEAR_CONTACTS });
@@ -88,11 +112,6 @@ const ContactsState = (props) => {
   // Clear Current Contact
   const clearCurrentContact = () => {
     dispatch({ type: CLEAR_CURRENT });
-  };
-
-  // Update Contact
-  const updateContact = (contact) => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
   };
 
   // Filter Contacts
